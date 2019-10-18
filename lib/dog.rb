d98ce0366e152ff067e47d1931b32b55c2e8b0e2
Mark Kauffman <markkauffman22@gmail.com>
2019-10-18 03:05:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,10 @@
 # Your code goes here!
 class Dog
-  def initialize 
-  @name = name
+  def initialize(name = "Fido") 
+    @name = name
+  end
   
-  
+  def name  # Getter
+    @ name = new_name
   
 end  
